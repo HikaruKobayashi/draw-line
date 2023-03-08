d89aff5e0b9f7ccd0fab18ded14aa850d5a60961
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2023-03-08 18:46:23 +0900

    refactoring item.js

--- src/item.js
+++ src/item.js
@@ -1,79 +1,42 @@
-export const createItem = () => {
-  // create container element
-  const container = document.createElement("div");
-  container.className = "draw-line-container";
-  document.body.appendChild(container);
+// create HTML elements
+const createElement = () => {
+  document.body.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="draw-line-container">
+        <ul>
+          <li class="draw-line-header">
+            <h1 class="draw-line-title">draw line</h1>
+            <button class="draw-line-close-button"></button>
+          </li>
+          <li>
+            <p class="draw-line-change-straight-line-button is-active">直線</p>
+            <p class="draw-line-change-square-button">四角</p>
+            <p class="draw-line-change-circle-button">丸</p>
+          </li>
+          <li class="draw-line-color-picker-wrapper">
+            <input type="color" class="draw-line-color-picker">
+          </li>
+          <li class="draw-line-footer">
+            <button class="draw-line-reset-button">リセット</button>
+          </li>
+        </ul>
+      </div>
+    `
+  );
+};
 
-  // create wrapper element
-  const wrapper = document.createElement("ul");
-  container.appendChild(wrapper);
-
-  // create header element
-  const header = document.createElement("li");
-  header.className = "draw-line-header";
-  wrapper.appendChild(header);
-
-  // create title element
-  const title = document.createElement("h1");
-  title.innerHTML = "draw line";
-  title.className = "draw-line-title";
-  header.appendChild(title);
-
-  // create close button element
-  const closeButton = document.createElement("button");
-  closeButton.className = "draw-line-close-button";
-  header.appendChild(closeButton);
-
-  // create line type wrapper element
-  const changeLineTypeWrapper = document.createElement("li");
-  wrapper.appendChild(changeLineTypeWrapper);
-
-  // create change straight line button element
-  const changeStraightLineButton = document.createElement("p");
-  changeStraightLineButton.innerHTML = "直線";
-  changeStraightLineButton.className =
-    "draw-line-change-straight-line-button is-active";
-  changeLineTypeWrapper.appendChild(changeStraightLineButton);
-
-  // create change square button element
-  const changeSquareButton = document.createElement("p");
-  changeSquareButton.innerHTML = "四角";
-  changeSquareButton.className = "draw-line-change-square-button";
-  changeLineTypeWrapper.appendChild(changeSquareButton);
-
-  // create change circle button element
-  const changeCircleButton = document.createElement("p");
-  changeCircleButton.innerHTML = "丸";
-  changeCircleButton.className = "draw-line-change-circle-button";
-  changeLineTypeWrapper.appendChild(changeCircleButton);
-
-  // create color picker wrapper element
-  const colorPickerWrapper = document.createElement("li");
-  colorPickerWrapper.className = "draw-line-color-picker-wrapper";
-  wrapper.appendChild(colorPickerWrapper);
-
-  // create color picker
-  const colorPicker = document.createElement("input");
-  colorPicker.type = "color";
-  colorPicker.className = "draw-line-color-picker";
-  colorPickerWrapper.appendChild(colorPicker);
-
-  // create footer element
-  const footer = document.createElement("li");
-  footer.className = "draw-line-footer";
-  wrapper.appendChild(footer);
-
-  // create reset button element
-  const resetButton = document.createElement("button");
-  resetButton.innerHTML = "リセット";
-  resetButton.className = "draw-line-reset-button";
-  footer.appendChild(resetButton);
-
-  // create style
+// add draw line styles
+const addDrawLineStyle = () => {
   const styleElement = document.createElement("link");
   styleElement.href = `${chrome.runtime.getURL("assets/css/index.css")}`;
   styleElement.rel = "stylesheet";
   document
     .getElementsByTagName("head")[0]
     .insertAdjacentElement("beforeend", styleElement);
+};
+
+export const createItem = () => {
+  createElement();
+  addDrawLineStyle();
 };
